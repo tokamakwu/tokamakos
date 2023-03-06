fda11135f593ec3aa7aa2b499907e9946e182257
casper <842590178@qq.com>
2023-03-06 23:14:10 +0800

    tokamak

---
+++ tokamak/initldr/include/ctrlhead.h
@@ -0,0 +1,7 @@
+#ifndef _CTRLHEAD_H
+#define _CTRLHEAD_H
+
+#include "type.h"
+#include "imgheader.h"
+
+#endif
---
+++ tokamak/initldr/ldr/inithead.c
@@ -0,0 +1,11 @@
+#include "ctrlhead.h"
+
+int inithead_entry()
+{
+    write_realintsvefile();
+    write_ldrfile();
+}
+
+void write_realintsvefile()
+{
+}
